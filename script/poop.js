8651b292a82fb69a9f1f603e723a713b13e45945
miliberlin <michelle.liebheit@posteo.de>
2021-04-06 16:43:55 +0200

    update sensibility of scoring

--- script/poop.js
+++ script/poop.js
@@ -1,21 +1,27 @@
 class Poop {
-    constructor() {
-        this.width = 30;
-        this.height = 30;
-        this.x = game.dog.x;
-        this.y = game.dog.y + game.dog.height + 5;
-        this.speed = 3;
-    }
-    draw() {
-        image(game.poopImage, this.x, this.y, this.width, this.height);
+  constructor() {
+    this.width = 30;
+    this.height = 30;
+    this.x = game.dog.x;
+    this.y = game.dog.y + game.dog.height + 5;
+    this.speed = 3;
+  }
+  draw() {
+    image(game.poopImage, this.x, this.y, this.width, this.height);
 
-        this.y += this.speed;
+    this.y += this.speed;
+  }
+  // increase score when poop is picked up
+  pickUpPoop() {
+    if (
+      ((game.player.y+10 <= this.y+this.height) &&
+      (game.player.y + game.player.height >= this.y + this.height))
+      &&
+      ((game.player.x <= this.x+this.height) &&
+      (game.player.x + game.player.height >= this.x + this.height))
+      ) {
+      game.score++;
+      return true;
     }
-    // increase score when poop is picked up
-    pickUpPoop() {
-        if (((this.y >= game.player.y+2) || (this.y >= game.player.y-2)) && ((this.x >= game.player.x+2) || (this.x >= game.player.x-2))) {
-            game.score++;
-            return true;
-        }
-    }
+  }
 }
