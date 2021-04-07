99bc216cec9a73e5b9b586e01c6671c12a6309db
miliberlin <michelle.liebheit@posteo.de>
2021-04-07 18:39:02 +0200

    adjust random movement of dog

--- script/background.js
+++ script/background.js
@@ -8,66 +8,53 @@ class Background {
     this.generateBackground();
   }
   generateBackground() {
-    for (let y = 0; y < 450; y+=30) {
+    for (let y = 0; y < HEIGHT+SQUARE+SQUARE; y+=30) {
       const row = [];
-      // console.log(y)
-      for (let x = 0; x < 450; x+=30) {
-        let obj = game.backgroundImages[Math.floor(Math.random()*game.backgroundImages.length)];
-        // let obj = game.backgroundImages[0]
-        // obj['x'] = x;
-        // obj['y'] = y;
-        // console.log('   ' +x)
-        // console.log(obj)
-        // console.log(row)
+      for (let x = 0; x < WIDTH; x+=30) {
+        let obj = {... game.backgroundImages[Math.floor(Math.random()*game.backgroundImages.length)] };
+        obj['x'] = x;
+        obj['y'] = y;
         row.push(obj)
-        // console.log(row)
       }
-      // console.log(row)
       this.backgroundArray.push(row);
     }
   }
   draw() {
-    // for (let row of this.backgroundArray) {
-    //   for (let tile of row) {
-    //     image(tile.src, tile.x, tile.y, SQUARE, SQUARE);
-    //   }
-    // }
-
-    // for (let tile of this.backgroundArray[8]) {
-    //   // console.log(tile)
-    //   image(tile.src, tile.x, tile.y, SQUARE, SQUARE);
-    // }
-
-    let row = this.baseRow;
-    let col = 0;
-    for (let y = 0; y < HEIGHT+SQUARE; y+=SQUARE) {
-      for (let x = 0; x < WIDTH; x+=SQUARE) {
-        let tile = this.backgroundArray[row][col];
-        tile['x'] = x;
-        tile['y'] = y;
-
-        image(tile.src, tile['x'], tile['y'], SQUARE, SQUARE);
-        col++;
-        if (col % 15 === 0) {
-          col = 0;
-        }
-      }
-      row++
-      if (row % 15 === 0) {
-        row = 0;
+    for (let row of this.backgroundArray) {
+      for (let tile of row) {
+        image(tile.src, tile.x, tile.y, SQUARE, SQUARE);
       }
     }
 
-    
+    // let row = this.baseRow;
+    // let col = 0;
+    // for (let y = 0; y < HEIGHT+SQUARE; y+=SQUARE) {
+    //   for (let x = 0; x < WIDTH; x+=SQUARE) {
+    //     let tile = this.backgroundArray[row][col];
+    //     tile['x'] = x;
+    //     tile['y'] = y;
 
-    // adding movement to the tiles by increasing the Y coordinates
-    // for (let row of this.backgroundArray) {
-    //   for (let tile of row) {
-    //     tile['y'] += this.speed;
-    //     if (tile['y'] > HEIGHT) {
-    //       tile['y'] = 0;
+    //     image(tile.src, tile['x'], tile['y'], SQUARE, SQUARE);
+    //     col++;
+    //     if (col % 15 === 0) {
+    //       col = 0;
     //     }
     //   }
+    //   row++
+    //   if (row % 15 === 0) {
+    //     row = 0;
+    //   }
     // }
+
+
+    // adding movement to the tiles by increasing the Y coordinates
+    for (let row of this.backgroundArray) {
+      for (let tile of row) {
+        tile['y'] += this.speed;
+        if ((tile['y']+SQUARE) === HEIGHT+25) {
+          tile['y'] = -25;
+        }
+      }
+    }
   }
 }
--- script/dog.js
+++ script/dog.js
@@ -6,28 +6,21 @@ class Dog {
         this.xArray = [0,30,60,90,120,150,180,210,240,270,300,330,360,390,420]
         this.y = this.height;
         this.stepCount = 0;
-        this.speed = 2;
+        this.speed = 1;
         this.poopArray = [];
         this.newPosition = this.xArray[Math.floor(Math.random() * (this.xArray.length))];
-        // this.newPosition = 300;
     }
     draw() {
         image(game.dogImage, this.x, this.y, this.width, this.height);
 
         // changing position
         if (this.newPosition < this.x) {
-            this.x -= this.speed;
-            // console.log(this.x)
-            // console.log(this.newPosition)
+            this.x -= Math.floor(random(3, this.speed));
         } else if (this.x < this.newPosition) {
-            this.x += this.speed*2;
-            // console.log(this.x)
-            // console.log(this.newPosition)
+            this.x += Math.floor(random(1, this.speed));
         }
         else {
             this.newPosition = this.xArray[Math.floor(random(0, this.xArray.length))];
-            // console.log(this.x)
-            // console.log(this.newPosition)
         }
 
         // changing sprites
